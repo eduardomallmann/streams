bde51253aabad0cce2e5165911e60aad566ff692
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-10-30 18:31:02 -0300

    Add trap examples

---
+++ traps/src/Reusing.java
@@ -0,0 +1,14 @@
+import java.util.stream.IntStream;
+
+public class Reusing {
+
+	public static void main(String[] args) {
+
+		IntStream stream = IntStream.of(1, 2);
+		
+		stream.forEach(System.out::println);
+
+		stream.forEach(System.out::println);
+
+	}
+}
