f78d08fb10f00779098c89fb0bba0af357a9b5df
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-10-30 22:30:43 -0300

    Add body to caso1

--- manipulando-listas/src/main/java/com/eduardomallmann/streams/manipulandolistas/ReportController.java
+++ manipulando-listas/src/main/java/com/eduardomallmann/streams/manipulandolistas/ReportController.java
@@ -1,14 +1,22 @@
 package com.eduardomallmann.streams.manipulandolistas;
 
+import com.eduardomallmann.streams.manipulandolistas.model.Catalogo;
 import com.eduardomallmann.streams.manipulandolistas.model.CatalogoRepository;
+import com.eduardomallmann.streams.manipulandolistas.model.Produto;
 import com.eduardomallmann.streams.manipulandolistas.model.ProdutoRepository;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @RestController
@@ -27,12 +35,38 @@ public class ReportController {
 	@GetMapping
 	public ResponseEntity<List<ReportResponse>> getReportFrom() {
 
+		Gson jsonConverter = new Gson();
+
 		// Catálogos válidos
+		Stream<Catalogo> catalogos = catalogoRepository.findAll().parallelStream()
+				.filter(catalogo -> catalogo.getAtivo() && catalogo.getDataValidadeCatalogo().after(new Date()));
+
+		List<String> catalogosIds = catalogos.map(Catalogo::getIdCatalogo).collect(Collectors.toList());
+
+		log.debug("Catalogos Ids: \n {}", jsonConverter.toJson(catalogosIds));
 
 		// Produtos dos catálogos válidos
+		Stream<Produto> produtos = produtoRepository.findAll().stream()
+				.filter(produto -> produto.getAtivo() && !produto.getPromocional() && catalogosIds.contains(produto.getIdCatalogo()));
+
+		Map<String, List<Produto>> mapaDeProdutos = produtos.collect(Collectors.groupingBy(Produto::getIdCatalogo));
+
+		log.debug("Produtos mapeados: \n {}", jsonConverter.toJson(mapaDeProdutos));
 
 		// Produtos ordenados por menor valor
+		List<ReportResponse> responseList = mapaDeProdutos.entrySet().parallelStream()
+				.map(mapa -> ReportResponse.builder()
+						.catalogo(mapa.getKey())
+						.produtos(mapa.getValue().stream().map(produto -> ProdutoResponse.builder()
+								.codigoProduto(produto.getCodigoProduto())
+								.precoVenda(produto.getPrecoVenda())
+								.build())
+								.sorted(Comparator.comparing(ProdutoResponse::getPrecoVenda)).collect(Collectors.toList()))
+						.build())
+				.collect(Collectors.toList());
 
-		return null;
+		log.debug("Resposta da requisição: \n {}", jsonConverter.toJson(responseList));
+
+		return ResponseEntity.ok(responseList);
 	}
 }
