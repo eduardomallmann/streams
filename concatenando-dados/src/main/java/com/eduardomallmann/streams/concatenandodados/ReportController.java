b1aa4fe5aece087307446aeacfa0cb153deed9b2
Eduardo Mallmann <mallmann.edu@gmail.com>
2018-10-31 01:12:07 -0300

    Add fix to initial body

--- concatenando-dados/src/main/java/com/eduardomallmann/streams/concatenandodados/ReportController.java
+++ concatenando-dados/src/main/java/com/eduardomallmann/streams/concatenandodados/ReportController.java
@@ -1,14 +1,14 @@
 package com.eduardomallmann.streams.concatenandodados;
 
-import com.eduardomallmann.streams.concatenandodados.models.*;
+import com.eduardomallmann.streams.concatenandodados.models.ClientRepository;
+import com.eduardomallmann.streams.concatenandodados.models.EmployeeRepository;
+import com.eduardomallmann.streams.concatenandodados.models.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/report")
@@ -29,16 +29,14 @@ public class ReportController {
 	public ResponseEntity<List<Person>> getReportList() {
 
 		// recuperar todos os funcionários
-		List<Employee> employees = employeeRepository.findAll();
+
 		// recuperar todos os clientes
-		List<Client> clients = clientRepository.findAll();
+
 		// concatenar as listas
-		Stream<Person> persons = Stream.concat(employees.stream(), clients.stream());
 
 		// filtrar por idade
-		List<Person> responseList = persons.filter(person -> person.getAge() >= MINIMUM_AGE).collect(Collectors.toList());
 
 
-		return ResponseEntity.ok(responseList);
+		return null;
 	}
 }
